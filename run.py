d2955696c0f71c934a54f1237faf42d8800cdacf
arnoldkoss <>
2023-11-25 16:23:28 +0000

    create function for the welcome message

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import random to generate random integers in game.
+"""
+from random import randint
+
+def welcome_page():
+    """
+    Displays a greeting.
+    Ascii art source:
+    https://patorjk.com/software/taag/#p=display&f=Doom&t=Battleship
+    """
+    print("""
+ _    _      _                          _ 
+| |  | |    | |                        | |
+| |  | | ___| | ___ ___  _ __ ___   ___| |
+| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ |
+\  /\  /  __/ | (_| (_) | | | | | |  __/_|
+ \/  \/ \___|_|\___\___/|_| |_| |_|\___(_)
+    """)
+    print("=" * 42 + "\n")
+    print("Welcome to Battleship! \n")
+welcome_page()    
