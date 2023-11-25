a5fa841c12c4cf18e22185b4530279920a3a1a93
arnoldkoss <>
2023-11-25 17:51:14 +0000

    create a validate name function to be shure the user uses only alphabetic characters

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ Import random to generate random integers in game.
 """
 from random import randint
 
+
+
 def welcome_message():
     """
     Displays a greeting.
@@ -19,13 +21,31 @@ def welcome_message():
     """)
     print("=" * 42 + "\n")
     print("Welcome to Battleship! \n")
-welcome_message()    
+    print("Board size: 6. Number of ships: 4 \n")
+    print("Top left corner is row: 0, col: 0  \n")
+
+welcome_message()
+
+def validate_name_input(name):
+    """Validate the user's name input."""
+    if not name:
+        print("Please enter a name.")
+        return False
+    elif not name.isalpha():
+        print("Name can only contain alphabetic characters.")
+        return False
+    else:
+        return True
 
 def input_name():
     """
-    Ask the user for a name and stores it
+    Get the user's name and store it
     in a global variable to be used in the game.
     """
     global user_name
-    user_name = input("Please enter your name: \n")
-input_name()        
+    while True:
+        user_name = input("Please enter your name: \n")
+        if validate_name_input(user_name):
+            break
+input_name() 
+print(f"Welcome, {user_name}!") 
